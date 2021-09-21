04c5e625ba0e6df9c26c425492cc5180e871c904
FaridRey <89561572+faridfr45@users.noreply.github.com>
2021-09-21 15:45:25 +0700

    Projects File

---
+++ AngryBirds/Assets/Scripts/ExplodeBird.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeBird : Bird
+{
+    public float explodeRadius = 1.38f;
+    public float force = 350f;
+
+    public LayerMask LayerToHit;
+
+    public GameObject efekLedakan;
+
+    public override void OnCollisionEnter2D(Collision2D other) {
+        Destroy(gameObject);
+        Explode();
+    }
+
+    void Explode(){
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, explodeRadius, LayerToHit);
+
+        foreach(Collider2D obj in objects){
+            Vector2 arah = obj.transform.position - transform.position;
+
+            obj.GetComponent<Rigidbody2D>().AddForce(arah * force);
+            if(obj.tag == "Enemy"){
+                Enemy musuh = obj.GetComponent<Enemy>();
+                musuh.Mati();
+            }
+        }
+        GameObject ledak = Instantiate(efekLedakan, transform.position, Quaternion.identity);
+        Destroy(ledak, 5);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explodeRadius);
+    }
+}
---
+++ AngryBirds/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void PlayLevel1(){
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void PlayLevel2(){
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
